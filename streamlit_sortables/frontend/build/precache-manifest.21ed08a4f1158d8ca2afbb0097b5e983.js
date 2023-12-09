e08f99e4ea9fe331ac7d183640fa7e236b0c8b06
Ryoma Kobayashi <ryoma.kobayashi@abejainc.com>
2023-12-10 02:37:24 +0900

    add: build files.

---
+++ streamlit_sortables/frontend/build/precache-manifest.21ed08a4f1158d8ca2afbb0097b5e983.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a047a62c93e7aa84cb5ecf981088f13",
+    "url": "./index.html"
+  },
+  {
+    "revision": "febbd5df97bc3c3ff2d4",
+    "url": "./static/css/main.1580ff0c.chunk.css"
+  },
+  {
+    "revision": "b0d7dd7d8d8ff587bc4f",
+    "url": "./static/js/2.dba1e32d.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.dba1e32d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "febbd5df97bc3c3ff2d4",
+    "url": "./static/js/main.b93dbfdb.chunk.js"
+  },
+  {
+    "revision": "af938b039e0e624810f1",
+    "url": "./static/js/runtime-main.09df5465.js"
+  }
+]);
